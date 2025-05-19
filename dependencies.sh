3a6ef6e4fe63616dbf96089939f409d2ae13283e
Patrick Meade <blinkdog@protonmail.com>
2025-05-18 23:03:36 -0500

    Initial workflows and placeholder main scene

---
+++ dependencies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# dependencies.sh
+
+export GODOT_VERSION="4.4.1"
+
+export GODOT_ENGINE_URL="https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}-stable/Godot_v${GODOT_VERSION}-stable_linux.x86_64.zip"
+
+export GODOT_EXPORT_TEMPLATE_URL="https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}-stable/Godot_v${GODOT_VERSION}-stable_export_templates.tpz"
